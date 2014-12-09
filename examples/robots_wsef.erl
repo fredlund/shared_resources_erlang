34b2cdcca0fc70e628511f9406d79b26fdfec485
fred <lfredlund@fi.upm.es>
2014-12-10 00:16:25 +0100

    Fixes.

--- examples/robots_wsef.erl
+++ examples/robots_wsef.erl
@@ -7,7 +7,7 @@
 -export([init/1,new_waiting/3,priority_enabled/4,post_waiting/4]).
 
 init([N,_MaxWeight]) ->
-  {ok,{N,lists:map(fun (I) -> {I,[]} end, lists:seq(1,N-1))}}.
+  {ok,{N,lists:map(fun (I) -> {I,[]} end, lists:seq(0,N-1))}}.
 
 new_waiting(_Call,WS,_DS) ->
   {void,WS}.
@@ -17,20 +17,25 @@ priority_enabled({call,exit,[_R,N,W]},_,{Max,Waiters},_) ->
     N==Max-1 ->
       true;
     true ->
-      {_,OldWaiters} = lists:keyfind(N+1,1,Waiters),
-      W == lists:max(OldWaiters)
-  end.
+      {_,OldWaiters} = lists:keyfind(N,1,Waiters),
+      W == lists:min(OldWaiters)
+  end;
+priority_enabled(_Call,_Info,_WS,_DS) ->
+  true.
 
 post_waiting({call,enter,[_R,N,W]},_CallInfo,WS={Max,Waiters},_) ->
   if
     N==Max-1 ->
       WS;
     true ->
-      {_,OldWaiters} = lists:keyfind(N+1,1,Waiters),
-      {Max,lists:keystore(N+1,1,Waiters,{N+1,[W|OldWaiters]})}
+      {_,OldWaiters} = lists:keyfind(N,1,Waiters),
+      {Max,lists:keystore(N,1,Waiters,{N,[W|OldWaiters]})}
   end;
-post_waiting(_,_,WS,_) ->
-  WS.
+post_waiting({call,exit,[_R,N,W]},_CallInfo,{Max,Waiters},_) ->
+  {_,OldWaiters} = lists:keyfind(N,1,Waiters),
+  {Max,lists:keystore(N,1,Waiters,{N,OldWaiters--[W]})}.
+
+  
 
 
 
