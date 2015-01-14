27ec0202a469407b38646ba2a2c59018cc05f4cf
fred <lfredlund@fi.upm.es>
2015-01-14 08:17:23 +0100

    Same number of warehouses as in the implementation.

--- examples/v1.erl
+++ examples/v1.erl
@@ -8,9 +8,9 @@ test() ->
 %%	 "/home/fred/practica_1_complete/G-4F1M/110175/20140605-170257",
 	 "/home/fred/practica_1_complete/G-4F1M/000999/20140522-143819/",
 	 "/home/fred/gits/src/cctester/test/cclib.jar"],
-  DataSpec = {robots,[3,1000]},
+  DataSpec = {robots,[4,1000]},
   WaitSpec = {fcfs,[]},
-  TestingSpec = {robot_commands,[10,3]},
+  TestingSpec = {robot_commands,[10,4]},
   tester:test(CP,Id,DataSpec,WaitSpec,TestingSpec).
 
 test2() ->
@@ -19,9 +19,9 @@ test2() ->
 %%	 "/home/fred/practica_1_complete/G-4F1M/110175/20140605-170257",
 	 "/home/fred/practica_1_complete/G-4F1M/000999/20140522-143819/",
 	 "/home/fred/gits/src/cctester/test/cclib.jar"],
-  DataSpec = {robots,[3,1000]},
+  DataSpec = {robots,[4,1000]},
   WaitSpec = {always,[]},
-  TestingSpec = {robot_commands,[10,3]},
+  TestingSpec = {robot_commands,[10,4]},
   tester:test(CP,Id,DataSpec,WaitSpec,TestingSpec).
   
 
