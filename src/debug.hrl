87e559046650c15492146007c4c7e952ffcadf8b
Lars-Ake Fredlund <lfredlund@fi.upm.es>
2016-07-04 14:02:03 +0200

    msg

--- src/debug.hrl
+++
@@ -1,6 +0,0 @@
--ifdef(debug).
--define(LOG(X,Y),
-	io:format("~s",[io_lib:format(X,Y)])).
--else.
--define(LOG(X,Y),true).
--endif.
--- src/resource_data_implementation.erl
+++
@@ -1,8 +0,0 @@
--module(resource_data_implementation).
-
--callback init([any()],any()) -> {ok,any()} | any().
--callback pre(resource:call(),any()) -> boolean().
--callback cpre(resource:call(),any()) -> boolean().
--callback post(resource:call(),any()) -> any().
-
-  
